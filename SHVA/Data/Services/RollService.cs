ca3285c83268326022e9e0312dff939d30363aa6
EmreCopur <emre.c.school@gmail.com>
2024-02-13 15:36:35 +0100

    fast

---
+++ SHVA/Data/Services/RollService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SHVA.Data.Services
+{
+    public class RollService(IDbContextFactory<ApplicationDbContext> dbContextFactory)
+    {
+        private IDbContextFactory<ApplicationDbContext> _dbContextFactory = dbContextFactory;
+
+      
+    }
+}
